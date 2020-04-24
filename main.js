cd301799cf35061d252e46e4dbdcaf8db875971d
Nomanna Oprah Mavata <nomannaoprah@gmail.com>
2020-04-24 13:26:54 +0200

    changes made

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ class Person {
     }
      //Creating a Method
     hello (){
-        return "Hello, my name is " + this.name + "and I am " + this.age + "years old. " + "My interests are " + this.interests;
+        return "Hello, my name is " + this.name + " and I am " + this.age + "years old. " + "My interests are" + this.interests;
     }
 
 }
 //creating new objects that are link to person class
 
-let person = new Person('Ryan', 30, 'male',[' being a hardarse ', 'agile', 'and ssd hard drives.'])
+let person = new Person('Ryan', 30, 'male',[' being a hardarse', ' agile', ' and ssd hard drives.'])
 let greeting = person.hello()
 
 console.log (greeting)
