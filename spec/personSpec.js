c91c6d3435414ba19e9de7264012894c7744b9fa
Nomanna Oprah Mavata <nomannaoprah@gmail.com>
2020-05-05 14:45:36 +0200

    edited Changes add to jasmine testing

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -1,29 +1,29 @@
 const { Person } = require("../src/person.js");
-let personObject = new Person("Ryan", 30, "male", [
+let person = new Person("Ryan", 30, "male", [
   " being a hardarse",
   " agile",
   " and ssd hard drives.",
 ]);
 
 describe("The Person class is being described", function () {
-  it("return the name property of the person object", function () {
-    expect(personObject.name).toBe("Ryan");
+  it("Should return the name property of the person object", function () {
+    expect(person.name).toBe("Ryan");
   });
-  it("return the age property of the person object", function () {
-    expect(personObject.age).toEqual(30);
+  it("Should return the age property of the person object", function () {
+    expect(person.age).toEqual(30);
   });
-  it("return the gender property of the person object", function () {
-    expect(personObject.gender).toBe("male");
+  it("Should return the gender property of the person object", function () {
+    expect(person.gender).toBe("male");
   });
-  it("return an array of interest from the person object", function () {
-    expect(personObject.interests).toEqual([
+  it("Should return an array of interest from the person object", function () {
+    expect(person.interests).toEqual([
       " being a hardarse",
       " agile",
       " and ssd hard drives.",
     ]);
   });
-  it("return the full content of greeting including the new Person object person and hello() method", function () {
-    expect(personObject.hello()).toBe(
+  it("Should return the full content of greeting including the new Person object person and hello() method", function () {
+    expect(person.hello()).toBe(
       "Hello, my name is Ryan and I am 30years old. My interests are being a hardarse, agile, and ssd hard drives."
     );
   });
